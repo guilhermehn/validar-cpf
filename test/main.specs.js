b719b20e018e1b3fd3c3ca7552022e18e71afb73
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2017-11-22 13:23:08 -0200

    Updated tests

---
+++ test/main.specs.js
@@ -0,0 +1,37 @@
+var validarCpf = require('../')
+var assert = require('assert')
+var gerarCpf = require('gerar-cpf')
+
+describe('validarCpf', function() {
+	var i = -1
+
+	it('should validate cpf', function() {
+		var cpfs = []
+
+		while (++i < 100) {
+			cpfs[i] = gerarCpf()
+		}
+
+		assert(
+			cpfs.every(function(cpf) {
+				return validarCpf(cpf)
+			})
+		)
+	})
+
+	it('should ignore non-numeric characters', function() {
+		var cpf = gerarCpf()
+		assert(validarCpf(cpf))
+	})
+
+	it('should ignore any type of mask', function() {
+		var cpf = gerarCpf('x-x-x-x-x-x-x-x-x-x-x', 'x')
+		assert(validarCpf(cpf))
+
+		cpf = gerarCpf('<><><><><><><><><><><>', '<>')
+		assert(validarCpf(cpf))
+
+		cpf = gerarCpf('___.___.___-__', '_')
+		assert(validarCpf(cpf))
+	})
+})
