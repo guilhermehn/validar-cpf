757e82074a684c2301ad4c968dc7d14fc3e42f16
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2019-03-03 02:25:14 -0300

    Moved tests to jest

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-'use strict'
+'use strict';
 
 const validarCpf = input => {
-	const cpf = input.replace(/\D/g, '')
+	const cpf = input.replace(/\D/g, '');
 
 	if (cpf === '' || cpf.length !== 11 || !/^\d{11}$/.test(cpf)) {
-		return false
+		return false;
 	}
 
-	const digits = cpf.split('').map(x => parseInt(x))
+	const digits = cpf.split('').map(x => parseInt(x));
 
 	for (let j = 0; j < 2; j++) {
-		let sum = 0
+		let sum = 0;
 
 		for (let i = 0; i < 9 + j; i++) {
-			sum += digits[i] * (10 + j - i)
+			sum += digits[i] * (10 + j - i);
 		}
 
-		let checkDigit = 11 - sum % 11
+		let checkDigit = 11 - (sum % 11);
 
 		if (checkDigit === 10 || checkDigit === 11) {
-			checkDigit = 0
+			checkDigit = 0;
 		}
 
 		if (checkDigit !== digits[9 + j]) {
-			return false
+			return false;
 		}
 	}
 
-	return true
-}
+	return true;
+};
 
-module.exports = validarCpf
+module.exports = validarCpf;
