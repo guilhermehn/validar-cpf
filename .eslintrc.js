6c5d341dd709e70807ad53c455d4443884feb42a
Guilherme Nagatomo <guilhermehn@users.noreply.github.com>
2019-03-03 03:07:26 -0300

    Chore/move to jest (#9)
    * Moved tests to jest
    
    * Updated travis targets
    
    * Updated eslint config
    
    * Added lint-staged and husky
    
    * Added jsdoc
    
    * Added travis coveralls step
    
    * Updated dependencies
    
    * Audit fix
    
    * Fixed test
    
    * Moved coverage reporter to coveralls script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,13 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true,
-        node: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-        sourceType: 'module',
-    },
-    rules: {
-        indent: ['error', 'tab'],
-        'linebreak-style': ['error', 'unix'],
-        quotes: ['error', 'single'],
-        semi: ['error', 'never'],
-    },
-}
+	env: {
+		commonjs: true,
+		es6: true,
+		jest: true
+	},
+	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single']
+	}
+};
---
+++ __tests__/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const validarCpf = require('..');
+const gerarCpf = require('gerar-cpf');
+
+describe('validarCpf', () => {
+	it('should validate cpf', () => {
+		for (let i = 0; i < 100; i++) {
+			expect(validarCpf(gerarCpf())).toBe(true);
+		}
+	});
+
+	it('should return false if cpf is invalid', () => {
+		expect(validarCpf('12345678900')).toBe(false);
+	});
+
+	it('should return false if input is empty', () => {
+		expect(validarCpf('')).toBe(false);
+	});
+
+	it('should return false if input have less than 11 characters', () => {
+		expect(validarCpf('1234567890')).toBe(false);
+	});
+
+	it('should return false if input have more than 11 characters', () => {
+		expect(validarCpf('123456789000')).toBe(false);
+	});
+
+	it('should return false if input doesn`t have 11 numbers', () => {
+		expect(validarCpf('1234567890a')).toBe(false);
+	});
+
+	it('should ignore any type of mask', () => {
+		expect(validarCpf(gerarCpf('x-x-x-x-x-x-x-x-x-x-x', 'x'))).toBe(true);
+		expect(validarCpf(gerarCpf('<><><><><><><><><><><>', '<>'))).toBe(true);
+		expect(validarCpf(gerarCpf('___.___.___-__', '_'))).toBe(true);
+	});
+});
