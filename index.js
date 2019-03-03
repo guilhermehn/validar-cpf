428bf0882d20e1c4c1ef33d6879cff709273655f
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2019-03-03 02:40:43 -0300

    Added jsdoc

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * Valida um número de CPF baseado no algorítmo "módulo 11"
+ * @param {String} input - Número de CPF a ser testado
+ * @return {Boolean}
+ * @example
+ * const validarCpf = require('validar-cpf');
+ * console.log(validarCpf('12345678900'));
+ */
 const validarCpf = input => {
 	const cpf = input.replace(/\D/g, '');
 
