ef17a5c27d3fbd18d538e6e9bd98caa942b8fb0c
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2020-04-06 14:43:12 -0300

    chore: Fix lint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const isRepeatingNumber = str => /^(\d)(\1){10}$/.test(str);
+const isRepeatingNumber = (str) => /^(\d)(\1){10}$/.test(str);
 
 /**
  * Valida um número de CPF baseado no algorítmo "módulo 11"
@@ -10,7 +10,7 @@ const isRepeatingNumber = str => /^(\d)(\1){10}$/.test(str);
  * const validarCpf = require('validar-cpf');
  * console.log(validarCpf('12345678900'));
  */
-const validarCpf = input => {
+const validarCpf = (input) => {
 	const cpf = input.replace(/\D/g, '');
 
 	if (
@@ -22,7 +22,7 @@ const validarCpf = input => {
 		return false;
 	}
 
-	const digits = cpf.split('').map(x => parseInt(x));
+	const digits = cpf.split('').map((x) => parseInt(x));
 
 	for (let j = 0; j < 2; j++) {
 		let sum = 0;
