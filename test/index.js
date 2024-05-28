47c4f39f564a703d150a6bd600a621a69a8f067a
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2024-05-28 13:50:32 -0300

    chore: replace mocha and chai with node native test runner

--- test/index.js
+++ test/index.js
@@ -1,45 +1,46 @@
 const validarCpf = require('..');
 const gerarCpf = require('gerar-cpf');
-const { expect } = require('chai');
+const { describe, it } = require('node:test');
+const assert = require('node:assert/strict');
 
 describe('validarCpf', () => {
 	it('should validate cpf', () => {
 		for (let i = 0; i < 100; i++) {
-			expect(validarCpf(gerarCpf())).equal(true);
+			assert(validarCpf(gerarCpf()));
 		}
 	});
 
 	it('should return false if cpf is invalid', () => {
-		expect(validarCpf('12345678900')).equal(false);
+		assert.strictEqual(validarCpf('12345678900'), false);
 	});
 
 	it('should return false if input is empty', () => {
-		expect(validarCpf('')).equal(false);
+		assert.strictEqual(validarCpf(''), false);
 	});
 
 	it('should return false if input have less than 11 characters', () => {
-		expect(validarCpf('1234567890')).equal(false);
+		assert.strictEqual(validarCpf('1234567890'), false);
 	});
 
 	it('should return false if input have more than 11 characters', () => {
-		expect(validarCpf('123456789000')).equal(false);
+		assert.strictEqual(validarCpf('123456789000'), false);
 	});
 
 	it('should return false if input doesn`t have 11 numbers', () => {
-		expect(validarCpf('1234567890a')).equal(false);
+		assert.strictEqual(validarCpf('1234567890a'), false);
 	});
 
 	it('should ignore any type of mask', () => {
-		expect(validarCpf(gerarCpf('x-x-x-x-x-x-x-x-x-x-x', 'x'))).equal(true);
-		expect(validarCpf(gerarCpf('<><><><><><><><><><><>', '<>'))).equal(true);
-		expect(validarCpf(gerarCpf('___.___.___-__', '_'))).equal(true);
+		assert(validarCpf(gerarCpf('x-x-x-x-x-x-x-x-x-x-x', 'x')));
+		assert(validarCpf(gerarCpf('<><><><><><><><><><><>', '<>')));
+		assert(validarCpf(gerarCpf('___.___.___-__', '_')));
 	});
 
 	it('should consider repeating numbers as invalid', () => {
-		expect(validarCpf('11111111111')).equal(false);
-		expect(validarCpf('22222222222')).equal(false);
-		expect(validarCpf('33333333333')).equal(false);
-		expect(validarCpf('44444444444')).equal(false);
-		expect(validarCpf('55555555555')).equal(false);
+		assert.strictEqual(validarCpf('11111111111'), false);
+		assert.strictEqual(validarCpf('22222222222'), false);
+		assert.strictEqual(validarCpf('33333333333'), false);
+		assert.strictEqual(validarCpf('44444444444'), false);
+		assert.strictEqual(validarCpf('55555555555'), false);
 	});
 });
